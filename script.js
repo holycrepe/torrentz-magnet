17ff51b40318a73f4cde4a8c208159641af1efb8
Avinash Puchalapalli <git@puchalapalli.com>
2016-07-01 22:21:22 -0400

    Process additional result pages

--- script.js
+++ script.js
@@ -62,13 +62,20 @@ Torrent.prototype = {
 	}
 };
 
-var pathName = location.pathname.replace(/\//g, '');
+var page = "other",
+	pathName = location.pathname.replace(/\//g, '');
+
 if (pathName.length === 40) {
-	var name = document.querySelector('.download h2 span').innerHTML.replace(/<[^>]*>/g, '');
-	var torrent = new Torrent(pathName, name, true);
-	document.querySelector('.download dl').insertAdjacentElement('afterend', torrent.linkElement);
-} else if (pathName.match(/search/i)) {
-	var searchItems = document.querySelectorAll('.results dl');
+	page = "torrent";
+} else {
+	var match = pathName.match(/^(search|any|my|verified|i$|i\/)/i);
+	if (match) {
+		page = match[1];
+	}
+}
+
+function processResults(results) {
+	var searchItems = results.querySelectorAll('dl');
 	for (var i = 0; i < searchItems.length; i++) {
 		var currentItem = searchItems[i];
 		var link = currentItem.querySelector('dt a');
@@ -76,7 +83,34 @@ if (pathName.length === 40) {
 
 		var dd = currentItem.querySelector('dd');
 		dd.insertBefore(torrent.linkElement, dd.firstChild);
-		
 		dd.style.width = (parseInt(window.getComputedStyle(dd).width) + 50) + 'px';
 	}
+}
+
+function processPage(run) {
+	run = run || 1;
+	var results = document.querySelectorAll('.results'),
+		total = results.length;
+	if (total) {
+		for (var i=0; i<total; i++) {
+			processResults(results[i]);
+		}
+		return;
+	}
+	if (run < 100) {
+		setTimeout(function() {
+			processPage(run + 1);
+		}, 1000);
+	} else {
+		console.log("Could not find Results... giving up");
+		debugger;
+	}
+}
+
+if (page === "torrent") {
+	var name = document.querySelector('.download h2 span').innerHTML.replace(/<[^>]*>/g, '');
+	var torrent = new Torrent(pathName, name, true);
+	document.querySelector('.download dl').insertAdjacentElement('afterend', torrent.linkElement);
+} else if (page !== "other") {
+	processPage();
 }
